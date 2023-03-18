a12b0793b5c35f2521b8416e973c8dd7e09746f7
arielx31 <yu.xie23@ycdsbk12.ca>
2023-03-18 15:18:24 -0400

    Snake segments + snake death

--- Assets/SnakeMovement.cs
+++ Assets/SnakeMovement.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class SnakeMovement : MonoBehaviour
 {
     private Vector2 direction = Vector2.right;
+    private List<Transform> segments;
+    public Transform segmentPrefab;
     
+    private void Start()
+    {
+        segments = new List<Transform>();
+        segments.Add(this.transform);
+    }
 
         
     private void Update()
@@ -31,6 +38,10 @@ public class SnakeMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        for (int i = segments.Count -1; i > 0; i--)
+        {
+            segments[i].position = segments[i - 1].position;
+        }
         this.transform.position = new Vector3(
                 Mathf.Round(this.transform.position.x) + direction.x,
                 Mathf.Round(this.transform.position.y) + direction.y,
@@ -38,5 +49,37 @@ public class SnakeMovement : MonoBehaviour
             );
         
             
+    }
+
+    private void Grow()
+    {
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = segments[segments.Count - 1].position; 
+        segments.Add(segment);
+    }
+
+    private void ResetState()
+    {
+        for (int i = 1; i < segments.Count; i++)
+        {
+            Destroy(segments[i].gameObject);
+        }
+
+        segments.Clear();
+        segments.Add(this.transform);
+        this.transform.position = Vector3.zero;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Food")
+        {
+            Grow();
+        } 
+        else if (other.tag == "Obstacle")
+        {
+            ResetState();
+        }
     }
 }
